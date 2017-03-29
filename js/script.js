8dc79c968868a37808a9bd7b2847f11cface81d3
dargaCode <darga.code@gmail.com>
2017-03-29 12:11:30 -0700

    Fix JSON path so it will work on web server

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 // CONSTANTS
 
-const TRIE_JSON_PATH = '/data/provider-trie.json';
+const TRIE_JSON_PATH = './data/provider-trie.json';
 const DEFAULT_RESULT_MESSAGE = 'results go here';
 const SEARCH_FAIL_MESSAGE = 'no results found';
 const NAME_CARD_DIV_CLASS = 'name-card';
